a4ff427a8c0e83ca70dcc408f245fa4d23f54e18
bms <mikesoylu@gmail.com>
2013-12-04 19:15:43 +0200

    working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,3 @@
 $(document).ready(function(){
-  var listings = new Listings([
-      new Listing({ title: 'Wet Cat'}),
-      new Listing({ title: 'Bitey Cat'}),
-      new Listing({ title: 'Surprised Cat'})
-  ]);
-
-  Estate.start({listings: listings});
-
-  listings.add(new Listing({
-    title: 'Cranky Cat',
-  }));
+  Estate.start();
 });
